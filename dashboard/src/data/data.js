338c976bb6bb8509fd2e3fa6d68b52e1eb227167
therafiniac <therafiniac@gmail.com>
2025-05-26 21:26:25 +0530

    complete dashboard

---
+++ dashboard/src/data/data.js
@@ -0,0 +1,21 @@
+
+export const positions = [
+  {
+    product: "CNC",
+    name: "INFY",
+    qty: 10,
+    avg: 1500.5,
+    price: 1520.2,
+    day: "+1.3%",
+    isLoss: false
+  },
+  {
+    product: "MIS",
+    name: "TCS",
+    qty: 5,
+    avg: 3200.0,
+    price: 3190.0,
+    day: "-0.3%",
+    isLoss: true
+  }
+];
